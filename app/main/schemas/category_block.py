4506b37b0e0df02195a4f2b736cb67dc014cd212
Ronny <noudjemambe@gmail.com>
2025-08-14 11:26:52 +0200

    new commit made

---
+++ app/main/schemas/category_block.py
@@ -0,0 +1,26 @@
+# from typing import Optional
+# from pydantic import BaseModel,configDict # pyright: ignore[reportMissingImports]
+# class CategoryBlock(BaseModel):
+#     name:str
+#     description:Optional[strt]
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#     class CategoryBlogResponse(BaseModel):
+#         uuid:str
+#         name:str
+#         description:Optional
+    
+    
+    
+    
+    
+    
